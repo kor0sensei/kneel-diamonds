6b38b8532f1b65f6bb7ca2d77065fd205bd64587
kor0sensei <averyolmstead@comcast.net>
2021-06-09 14:54:17 -0500

    added event click listener for user selected options, replaced for loop with .map and .join array methods for html representation of data

--- scripts/JewelryStyles.js
+++ scripts/JewelryStyles.js
@@ -5,20 +5,26 @@ const styles = getStyles()
 document.addEventListener(
     "change",
     (event) => {
+        if (event.target.name === "style") {
+            window.alert(`User Chose Style ${event.target.value}`)
+        }
     }
 )
 
 export const JewelryStyles = () => {
     let html = "<ul>"
 
-    // Use .map() for converting objects to <li> elements
-    const listItemsArray =
-
-
-    // Join all of the strings in the array into a single string
+    const listItems = styles.map(
+    (style) => {
+        return `<li>
+            <input type="radio" name="style" value="${style.id}" /> ${style.style}
+        </li>`
+    }
+)
     html += listItems.join("")
-
     html += "</ul>"
+
+    console.log(listItems.join(""))
     return html
 }
 
--- scripts/database.js
+++ scripts/database.js
@@ -33,10 +33,39 @@ const database = {
             styleId: 3,
             timestamp: 1614659931693
         }
-    ]
-}
+    ],
+    orderBuilder: {
+        metalId: "",
+        sizeId: "",
+        styleId:""
+    }
+        
+    }
 
 export const getMetals = () => {
     return database.metals.map(metal => ({...metal}))
 }
 
+export const getSizes = () => {
+    return database.sizes.map(size => ({...size}))
+}
+
+export const getStyles = () => {
+    return database.styles.map(style => ({...style}))
+}
+
+export const getOrders = () => {
+    return database.orders.map(order => ({...order}))
+}
+
+export const setMetal = (id) => {
+    database.orderBuilder.metalId = id
+}
+
+export const setSize = (id) => {
+    database.orderBuilder.sizeId = id
+}
+
+export const setStyle = (id) => {
+    database.orderBuilder.styleId = id
+}
